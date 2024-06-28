2e61f7072b861ce0c7359f63eb48c709e1a4ac65
David-McCaig <davidmccaig1@gmail.com>
2024-06-28 12:29:00 +1200

    Create hero and nav bar section

---
+++ src/app/components/hero.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import PrimaryButton from './primary-button'
+
+export default function hero() {
+  return (
+    <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
+    <div className="container flex max-w-[74rem] flex-col items-center gap-4 text-center">
+      <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
+        SoundBright <br />A Voice{" "}
+        <span className="bg-gradient-to-r from-cyan-500 to-blue-500 text-transparent bg-clip-text">
+          {" "}
+          Visualizer
+        </span>
+      </h1>
+      <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+      Watch as our 3D globe changes from red to yellow to green, giving you instant, easy-to-understand feedback on your speech volume.
+      </p>
+      <div className="space-x-4">
+        <PrimaryButton>Get Started</PrimaryButton>
+      </div>
+    </div>
+  </section>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,40 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import PrimaryButton from "./components/primary-button";
+import Hero from "./components/hero";
+import { Header } from "./components/Header";
+
 export default function Home() {
   return (
-    <main className="">
-      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
-        <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
-          <Link
-            href={""}
-            className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
-            target="_blank"
-          >
-            Follow along on Twitter
-          </Link>
-          <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
-            An example app built using Next.js 13 server components.
-          </h1>
-          <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-            I&apos;m building a web app with Next.js 13 and open sourcing
-            everything. Follow along as we figure this out together.
-          </p>
-          <div className="space-x-4">
-            <Link href="/login" className="">
-              Get Started
-            </Link>
-            <Link
-              href={""}
-              target="_blank"
-              rel="noreferrer"
-              className=""
-            >
-              GitHub
-            </Link>
-          </div>
-        </div>
-      </section>
+    <main className="bg-slate-50">
+      <Header/>
+      <Hero/>
+      
     </main>
   );
 }
